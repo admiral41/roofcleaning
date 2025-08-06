f56ff70e350d7e4df8afcb8f009722dc0ca2a06e
Susan Kafle <kafle.susan671@outlook.com>
2025-08-06 09:20:44 +0545

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Add your rules override here:
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-    },
-  },
 ];
 
 export default eslintConfig;
